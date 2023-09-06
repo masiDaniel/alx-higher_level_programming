cf0c3643db98729682ab01cdcd92c841724bb350
azte-c <masidaniel02@gmail.com>
2023-09-06 22:26:11 +0300

    code chhange

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,7 +3,6 @@ word = "Holberton"
 first_3 = word[:3]
 last_2 = word[-2:]
 middle = word[1:-1]
-
 print("First 3 letters: {}" .format(first_3))
 print("last 2 letters: {}" .format(last_2))
 print("middle word: {}" .format(middle))
