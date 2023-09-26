c8a45838c84732b41050dba00ea5471a1d95ec80
azte-c <masidaniel02@gmail.com>
2023-09-26 22:55:14 +0300

    documentation error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
+    """prints the first elements in a list
+
+    Args:
+        my_list(list): the list being printed from
+        x(int): no of elements
+
+    Returns:
+        no of elements
+    """
     num = 0
     for a in range(0, x):
         try:
