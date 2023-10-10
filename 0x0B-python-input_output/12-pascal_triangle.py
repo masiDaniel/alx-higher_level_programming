8eccf41c95da66d970a3c6179df582307fd03d55
azte-c <masidaniel02@gmail.com>
2023-10-11 00:38:51 +0300

    minor loop error

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -15,7 +15,7 @@ def pascal_triangle(n):
         prev_row = p_triangle[-1]
         new_row = [1]
 
-        for j in range(1, i):
+        for j in range(1, x):
             new_row.append(prev_row[j - 1] + prev_row[j])
         new_row.append(1)
         p_triangle.append(new_row)
